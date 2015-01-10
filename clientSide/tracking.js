06cfd14dae2146c236019f11841910b210c46f26
Conal Mullan <ffxxrr@gmail.com>
2015-01-10 12:22:09 -0800

    find all ad iframes / add mouseover/out console mgs / trigger alert on 'blur'

--- clientSide/tracking.js
+++ clientSide/tracking.js
@@ -1,8 +1,9 @@
 // borrowed from : http://www.bennadel.com/blog/1752-tracking-google-adsense-clicks-with-jquery-and-coldfusion.htm
 // obviously requires jquery 
 
-jQuery(function( $ ){
+$( document ).ready(function() {
 
+	console.log('initialized tracking script');
 	// I am a flag that will determine if the user is
 	// currently mousing over a Google AdSense.
 	var isOverGoogleAd = false;
@@ -12,18 +13,46 @@ jQuery(function( $ ){
 	// current window blurs, we can guesstimate as to
 	// whether or not the window-blur was due to the user
 	// clicking the Google ad.
+
+	// we want to support muliple ad providers rendering content in iframes
+	// we are interested in where in the page the ads appear. 
+	// 
+	$('iframe').each(function()
+	{
+     	console.log($(this).attr('id'));
+     	if( $(this).attr('id').match(/aswift/) ) 
+     	{	        	        
+	        $(this).mouseover(
+		        function(){
+		            console.log('over');
+		            isOverGoogleAd = true;
+		        }
+		    );
+		    $(this).mouseout(
+		        function(){
+		            isOverGoogleAd = false;
+		            console.log('out');
+		        }
+		    );
+     	}
+  	});
+	
+
+/*
 	$( "iframe[ id *= google ]" )
 	    .mouseover(
 	        function(){
+	            console.log('over');
 	            isOverGoogleAd = true;
 	        }
 	    )
 	    .mouseout(
 	        function(){
 	            isOverGoogleAd = false;
+	            console.log('out');
 	        }
 	    );
-
+*/
 	// Now that we are tracking the mouse movements over
 	// the Google AdSense, let's track the window's blur
 	// event to see if we can guesstimate the AdSesnse
@@ -39,13 +68,19 @@ jQuery(function( $ ){
 	            // was blurred, it is reasonable to
 	            // estimate that the blurring is due to
 	            // the user clicking one of the ads.
+	            
+	            // we want to invoke google tag manager here
+	            // lets have an alert for know for some testing
+	            alert('looks like you clicked an ad! - now I just have to work out which one and where!');
+
+	            /*
 	            $.ajax({
 	                type: "post",
 	                url: "track.cfm",
 	                data: {
 	                    adUrl: window.location.href
 	                    }
-	            });
+	            }); */
 
 	        }
 	    }
